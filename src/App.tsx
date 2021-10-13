ef52ecbbf7a3bad89508943ce5e5efcbeab35ca4
aroquev00 <aroquev00@users.noreply.github.com>
2021-10-12 21:18:13 -0500

    Add initial menu account.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,30 @@ import Browse from "./modules/browse/components/Browse";
 import Dashboard from "./modules/dashboard/components/Dashboard";
 import AppBar from "./modules/navigation/components/AppBar";
 
+export interface User {
+  id: string;
+  name: string;
+  img: string;
+}
+
+export interface AccountMenuProps {
+  user: User;
+  updateUser: () => void;
+}
+
 export default function App() {
+  let mockUser: User = { id: "someId", name: "Juan", img: "someURL" };
+
+  const [user, setUser] = React.useState<User>({ id: "", name: "", img: "" });
+
+  const toggleUser = () => {
+    setUser(mockUser);
+  };
+
   return (
     <Router>
       <div>
-        <AppBar />
+        <AppBar {...{ user: user, updateUser: toggleUser }} />
         <Switch>
           <Route path="/browse" exact>
             <Browse />
--- src/modules/navigation/components/AppBar.tsx
+++ src/modules/navigation/components/AppBar.tsx
@@ -4,14 +4,22 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { NavTabs } from "./Tabs";
+import AccountMenu from "../../account/components/AccountMenu";
+import { AccountMenuProps } from "../../../App";
 
-export default function CustomAppBar() {
+export default function CustomAppBar(props: AccountMenuProps) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ backgroundColor: "white" }}>
         <Toolbar>
           <NavTabs />
-          <Button color="primary">Login</Button>
+          {props.user.id !== "" ? (
+            <AccountMenu {...props} />
+          ) : (
+            <Button color="primary" onClick={props.updateUser}>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
