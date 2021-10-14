fe4b4314e32dff2d7f2f6b2df4fd612b04a0e467
dfnerio <diego9816@hotmail.com>
2021-10-14 10:21:25 -0500

    add login page

---
+++ src/modules/auth/components/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card, Container } from "@mui/material";
+
+export default function Login() {
+  return (
+    <Container maxWidth="lg">
+      <Card title="Login" />
+    </Container>
+  );
+}
