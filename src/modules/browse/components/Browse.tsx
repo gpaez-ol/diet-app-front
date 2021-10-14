12cc4ee4264fe7ddf9e562af4332702c5fc09a22
aroquev00 <aroquev00@users.noreply.github.com>
2021-10-13 19:00:08 -0500

    Add dummy pagination menu.

--- src/modules/browse/components/Browse.tsx
+++ src/modules/browse/components/Browse.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@mui/material";
 import React from "react";
+import DietPagination from "./DietPagination";
 
 export default function Browse() {
   return (
     <Container maxWidth="lg">
       <h1>You're in Browse</h1>
+      <DietPagination />
     </Container>
   );
 }
