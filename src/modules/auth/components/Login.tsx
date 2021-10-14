2d880413acae8d67bb8785382b699add6a3a5fcc
dfnerio <diego9816@hotmail.com>
2021-10-14 12:57:20 -0500

    add login functionality

--- src/modules/auth/components/Login.tsx
+++ src/modules/auth/components/Login.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { Card, Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import LoginForm from "./LoginForm";
 
 export default function Login() {
+  const browserHistory = useHistory();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      browserHistory.push("/");
+    }
+  });
+
   return (
-    <Container maxWidth="lg">
-      <Card title="Login" />
+    <Container maxWidth="md">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <LoginForm />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
