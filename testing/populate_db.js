7d95bc0e291efd6af63cdd89048c2cc2755e13d1
aroquev00 <aroquev00@users.noreply.github.com>
2021-11-03 20:29:10 -0600

    Add db population.

--- testing/populate_db.js
+++ testing/populate_db.js
@@ -1,5 +1,19 @@
+const Retrieve = require('./retrieve') 
 const axios = require('axios');
 
+const post_objects = (objects, url) => {
+  objects.forEach(async (object) => {
+    await axios
+      .post(url, object)
+      .then(res => {
+        console.log(`statusCode: ${res.status}`)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  });
+}
+;
 // Create categories
 const upload_categories = () => {
   const test_categories = [
@@ -25,18 +39,10 @@ const upload_categories = () => {
     },
   ];
 
-  test_categories.forEach((category) => {
-    axios
-      .post('http://algofit-qa-alb-599938117.us-east-1.elb.amazonaws.com/category', category)
-      .then(res => {
-        console.log(`statusCode: ${res.status}`)
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-  });
+  post_objects(test_categories, 'http://algofit-qa-alb-599938117.us-east-1.elb.amazonaws.com/category');
 };
+
+
 
 // Create ingredients
 const upload_ingredients = () => {
@@ -52,70 +58,124 @@ const upload_ingredients = () => {
     { "name": "Lettuce" }
   ];
   
-  test_ingredients.forEach((ingredient) => {
-    axios
-      .post('http://algofit-qa-alb-599938117.us-east-1.elb.amazonaws.com/ingredient', ingredient)
-      .then(res => {
-        console.log(`statusCode: ${res.status}`)
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-  });
+  post_objects(test_ingredients, 'http://algofit-qa-alb-599938117.us-east-1.elb.amazonaws.com/ingredient');
 };
 
-
 // Create meals
-const upload_meals = () => {
+const upload_meals = (ingredients) => {
+  const get_random_ingredients = () => {
+    const shuffled = [...ingredients].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, Math.floor(Math.random() * ingredients.length)).map((ingredient) => ({ 
+        "ingredientId": ingredient.id,
+        "amount": Math.floor(Math.random() * 10),
+        "notes": "No notes"
+    }));
+  };
+
   const test_meals = [
     {
       "name": "Fruit salad",
       "kilocalories": 100,
-      "preparation": "Cut a lot of fruit and mix it with lettuce."
+      "preparation": "Cut a lot of fruit and mix it with lettuce.",
+      "mealIngredients": get_random_ingredients()
     },
     {
       "name": "Pancakes",
       "kilocalories": 300,
-      "preparation": "Make some pancakes."
+      "preparation": "Make some pancakes.",
+      "mealIngredients": get_random_ingredients()
     },
     {
       "name": "Trail mix",
       "kilocalories": 150,
-      "preparation": "Mix nuts with almonds and cherry."
+      "preparation": "Mix nuts with almonds and cherry.",
+      "mealIngredients": get_random_ingredients()
     },
     {
       "name": "Yoghurt",
       "kilocalories": 220,
-      "preparation": "Mix greek yoghurt with almonds and apple."
+      "preparation": "Mix greek yoghurt with almonds and apple.",
+      "mealIngredients": get_random_ingredients()
     },
     {
       "name": "BBQ",
       "kilocalories": 700,
-      "preparation": "Roast the meat."
+      "preparation": "Roast the meat.",
+      "mealIngredients": get_random_ingredients()
     }
   ];
+
+  post_objects(test_meals, 'http://algofit-qa-alb-599938117.us-east-1.elb.amazonaws.com/meal');
 };
 
 // Create diets
-const upload_diets = () => {
+const upload_diets = (categories, meals) => {
+  const get_random_categories = () => {
+    const shuffled = [...categories].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, Math.floor(Math.random() * categories.length)).map((category) => category.id);
+  };
+
+  const get_random_meals = () => {
+    let random_meals = [];
+
+    for (let i = 0; i < 10; i++) {
+      random_meals.push({
+        "id": meals[Math.floor(Math.random() * meals.length)].id,
+        "mealNumber": i
+      });
+    }
+    return random_meals;
+  };
+
   const test_diets = [
     {
       "name": "Mediterranean",
       "description": "The Mediterranean diet may offer a host of health benefits, including weight loss, heart and brain health, cancer prevention, and diabetes prevention and control. By following the Mediterranean diet, you could also keep that weight off while avoiding chronic disease.",
       "type": 0,
-      "categoryIds": [
-        "08d99bee-60f5-4429-89cb-935e401a9b93"
-      ],
-      "dietMeals": [
-        {
-          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-          "mealNumber": 0
-        }
-      ]
+      "categoryIds": get_random_categories(),
+      "dietMeals": get_random_meals()
+    },
+    {
+      "name": "Italian",
+      "description": "Lots of pasta",
+      "type": 0,
+      "categoryIds": get_random_categories(),
+      "dietMeals": get_random_meals()
+    },
+    {
+      "name": "Mexican",
+      "description": "Viva Mexico",
+      "type": 0,
+      "categoryIds": get_random_categories(),
+      "dietMeals": get_random_meals()
+    },
+    {
+      "name": "Ketto",
+      "description": "Low fat.",
+      "type": 0,
+      "categoryIds": get_random_categories(),
+      "dietMeals": get_random_meals()
+    },
+    {
+      "name": "Dash",
+      "description": "Eat like ancestors.",
+      "type": 0,
+      "categoryIds": get_random_categories(),
+      "dietMeals": get_random_meals()
     }
   ];
+  //console.log(test_diets);
+  post_objects(test_diets, 'http://algofit-qa-alb-599938117.us-east-1.elb.amazonaws.com/diet');
 };
 
-// Call the appropriate function to upload the desired items
-upload_ingredients()
+const doStuff = async () => {
+  upload_categories();
+  const categories = await Retrieve.retrieve_categories(); 
+  upload_ingredients();
+  const ingredients = await Retrieve.retrieve_ingredients();
+  upload_meals(ingredients);
+  const meals = await Retrieve.retrieve_meals();
+  upload_diets(categories, meals);
+}
+
+doStuff();
