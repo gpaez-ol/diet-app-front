1d48ca3d92da5f9b45addd571a13eff37f0b4266
aroquev00 <aroquev00@users.noreply.github.com>
2021-11-04 11:06:06 -0600

    Add backend category retrieval.

--- src/modules/browse/types/DietCategory.tsx
+++ src/modules/browse/types/DietCategory.tsx
@@ -1,3 +1,5 @@
 export default interface DietCategory {
+  id: string,
   name: string,
+  imageRef: string,
 }
--- src/modules/general/utils/urls.tsx
+++ src/modules/general/utils/urls.tsx
@@ -4,4 +4,6 @@ export const URLs = {
   login: baseUrl + "/login",
   biometric: baseUrl + "/biometric",
   signup: baseUrl + "/signup",
+  category: baseUrl + "/category",
+  diet: baseUrl + "/diet",
 };
