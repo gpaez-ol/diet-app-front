abf4c0976751ee0bc00fa1c6030470091cb66a99
Armando Roque <43384970+aroquev00@users.noreply.github.com>
2021-11-11 08:12:09 -0600

    Add diet preview modal (#30)
    * Add diet preview modal.
    
    * Add href link to DietInfoCard.
    
    * Change location of DietInfoCard.
    
    * Add chip to DietInfoCard.
    
    Co-authored-by: aroquev00 <aroquev00@users.noreply.github.com>

---
+++ src/modules/general/components/MealsList.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Diet from "../interfaces/Diet";
+
+export default function MealsList(diet: Diet) {
+  return (
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      {diet.meals.map((meal, index) => {
+        const caloriesMsg = meal.kilocalories + " kilocalories";
+        return (
+          <ListItem key={index}>
+            <ListItemAvatar>
+              <Avatar>{index + 1}</Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={meal.name} secondary={caloriesMsg} />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
