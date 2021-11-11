367749660eb07d7383db17f44f55dd09f4ef5dde
aroquev00 <aroquev00@users.noreply.github.com>
2021-11-10 21:12:48 -0600

    Add chip to DietInfoCard.

--- src/modules/general/components/DietInfoCard.tsx
+++ src/modules/general/components/DietInfoCard.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
 import Diet from "../../general/interfaces/Diet";
@@ -31,20 +33,20 @@ export default function DietInfoCard(diet: Diet) {
         <Typography variant="body2" color="text.secondary">
           {diet.description}
         </Typography>
+        <Box sx={{ mb: 1 }}>
         <Typography variant="h6" component="div">
           Diet categories
         </Typography>
         <Stack direction="row" spacing={1}>
           {diet.categories.map((category) => {
             return (
-              <Chip
-                key={category.id}
-                label={category.name}
-                variant="outlined"
-              />
+              <Tooltip key={category.id} title={category.description} arrow>
+                  <Chip label={category.name} variant="outlined" />
+                </Tooltip>
             );
           })}
         </Stack>
+        </Box>
         <Typography variant="h6" component="div">
           Meals in this diet
         </Typography>
